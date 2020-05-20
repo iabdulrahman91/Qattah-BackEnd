226fccd7ac198064e950a57d4199f5fdff71e83e
iabdulrahman91 <aalana10@mail.depaul.edu>
2020-05-20 20:14:41 +0300

    EventAdmin middleware added and enforce for Event Deletion.

---
+++ app/Http/Middleware/EventAdmin.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class EventAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $user = Auth::user();
+        if ($user->id == $request->event->admin->id) {
+            return $next($request);
+        }
+        return response()
+            ->json(['message' => 'Forbidden'])
+            ->setStatusCode(403);
+    }
+}
