34f0d3e60bf289f047777d911b9fb08e5702d62d
iabdulrahman91 <aalana10@mail.depaul.edu>
2020-05-30 10:00:01 +0300

    added privacy and support pages

---
+++ resources/views/privacy.blade.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Qattah | قطّة</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 5px;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 84px;
+        }
+
+        .links > a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
+</head>
+<body>
+<div class="m-b-md">
+    <p align="center"><strong>MOBILE APP PRIVACY POLICY</strong></p>
+
+    <p></p>
+
+    <h1 align="center">Qattah | قطّة</h1>
+
+    <p style="text-align: center;">Last updated May 30, 2020</p>
+
+    <p></p>
+
+    <p>We, Qattah | قطّة Developer, respects the privacy of our users. This Privacy Policy explains how we collect, use,
+        disclose, and safeguard your information when you visit our mobile application (the “Qattah | قطّة”). &nbsp;&nbsp;Please
+        read this Privacy Policy carefully.&nbsp; IF YOU DO NOT AGREE WITH THE TERMS OF THIS PRIVACY POLICY, PLEASE DO
+        NOT ACCESS/INSTALL THE APPLICATION.</p>
+
+    <p></p>
+
+    <p>We reserve the right to make changes to this Privacy Policy at any time and for any reason.&nbsp; We will alert
+        you about any changes by updating the “Last updated” date of this Privacy Policy.&nbsp; You are encouraged to
+        periodically review this Privacy Policy to stay informed of updates. You will be deemed to have been made aware
+        of, will be subject to, and will be deemed to have accepted the changes in any revised Privacy Policy by your
+        continued use of the Application after the date such revised Privacy Policy is posted.&nbsp;</p>
+
+    <p></p>
+
+    <p>This Privacy Policy does not apply to the third-party online/mobile store from which you install the Application
+        or make payments, including any in-app purchases, which may also collect and use data about you.&nbsp; We are
+        not responsible for any of the data collected by any such third party.</p>
+
+    <h1>COLLECTION OF YOUR INFORMATION</h1>
+
+    <p></p>
+
+    <p>We may collect information about you in a variety of ways. The information we may collect via the Application
+        depends on the content and materials you use, and includes: </p>
+
+    <h2>Personal Infromation</h2>
+    <p>Identifiable information (such as your name, phone number and email address) that you
+        give to us when choosing to participate in various activities related to the Application, such as
+        creating events, invite member to event and post payments and purchases. If you choose to share data about
+        yourself via your profile, online chat, or other interactive areas of
+        the Application, please be advised that all data you disclose in these areas is public and your data will be
+        accessible to anyone who accesses the Application.</p>
+
+    <h1>USE OF YOUR INFORMATION</h1>
+
+    <p>Having accurate information about you permits us to provide you with a smooth, efficient, and customized
+        experience. Specifically, we may use information collected about you via the Application to: </p>
+
+    <p>
+    <ul>
+        <li>Compile anonymous statistical data and analysis for use internally or with third parties.</li>
+        <li>Email you regarding your account or order.</li>
+        <li>Enable user-to-user interaction.</li>
+        <li>Generate a personal profile about you to make future visits to the Application more personalized.</li>
+        <li>Increase the efficiency and operation of the Application.</li>
+        <li>Monitor and analyze usage and trends to improve your experience with the Application.</li>
+        <li> Notify you of updates to the Application.</li>
+        <li>Offer new products, services, mobile applications, and/or recommendations to you.</li>
+        <li>Perform other business activities as needed.</li>
+    </ul>
+    </p>
+
+    <p></p>
+
+    <h1>DISCLOSURE OF YOUR INFORMATION</h1>
+
+    <p>We may share information we have collected about you in certain situations. Your information may be disclosed as
+        follows: </p>
+
+    <h2>By Law or to Protect Rights</h2>
+    <p>If we believe the release of information about you is necessary to respond to legal process, to investigate or
+        remedy potential violations of our policies, or to protect the rights, property, and safety of others, we may
+        share your information as permitted or required by any applicable law, rule, or regulation. This includes
+        exchanging information with other entities for fraud protection and credit risk reduction.</p>
+
+    <h2>Interactions with Other Users</h2>
+    <p>
+        If you interact with other users of the Application, those users may see your name, profile photo, and
+        descriptions of your activity, including sending invitations to other users, events, and friendship requests.
+    </p>
+    <h1>THIRD-PARTY WEBSITES</h1>
+
+    <p>The Application may contain links to third-party websites and applications of interest, including advertisements
+        and external services, that are not affiliated with us. Once you have used these links to leave the Application,
+        any information you provide to these third parties is not covered by this Privacy Policy, and we cannot
+        guarantee the safety and privacy of your information. Before visiting and providing any information to any
+        third-party websites, you should inform yourself of the privacy policies and practices (if any) of the third
+        party responsible for that website, and should take those steps necessary to, in your discretion, protect the
+        privacy of your information. We are not responsible for the content or privacy and security practices and
+        policies of any third parties, including other sites, services or applications that may be linked to or from the
+        Application.</p>
+
+    <h1 align="left">POLICY FOR CHILDREN</h1>
+
+    <p>We do not knowingly solicit information from or market to children under the age of 13. If you become aware of
+        any data we have collected from children under age 13, please contact us using the contact information provided
+        below.</p>
+
+    <h1>CONTROLS FOR DO-NOT-TRACK FEATURES&nbsp;</h1>
+
+    <p>Most web browsers and some mobile operating systems [and our mobile applications] include a Do-Not-Track (“DNT”)
+        feature or setting you can activate to signal your privacy preference not to have data about your online
+        browsing activities monitored and collected.&nbsp; No uniform technology standard for recognizing and
+        implementing DNT signals has been finalized. As such, we do not currently respond to DNT browser signals or any
+        other mechanism that automatically communicates your choice not to be tracked online.&nbsp; If a standard for
+        online tracking is adopted that we must follow in the future, we will inform you about that practice in a
+        revised version of this Privacy Policy.&nbsp;</p>
+
+    <h1 align="left"></h1>
+
+    <h1 align="left">CONTACT US</h1>
+
+    <p>If you have questions or comments about this Privacy Policy, please contact us at:</p>
+
+    <p></p>
+
+    <p>Abdulrahman Alanazi</p>
+
+    <p>info@iAbdul.tech</p>
+
+    <p></p>
+</div>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/privacy', function () {
+    return view('privacy');
+});
+Route::get('/support', function () {
+    return view('support');
+});
